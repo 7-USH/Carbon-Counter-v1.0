e9accfa999942b6b08ecb1bfda283e3df60dba2f
Tushar <tush1245@gmail.com>
2021-12-02 18:39:52 +0530

    effects added

--- lib/models/button.dart
+++ lib/models/button.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: non_constant_identifier_names, avoid_types_as_parameter_names
+
 import 'package:carbon_counter/constants/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -10,35 +12,50 @@ class CustomButton extends StatefulWidget {
 }
 
 class _CustomButtonState extends State<CustomButton> {
+  Color newColor = secondaryShrinePink;
+  bool onPressed = false;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: (){
+      onTap: () {
+       
+        //TODO: Firestore....
 
-        // Calculation Button
 
+      },
+      onTapDown: (TapDownDetails) {
+        onPressed = !onPressed;
+        setState(() {});
+      },
+      onTapUp: (TapUpDetails) {
+        onPressed = false;
+        setState(() {
+          
+        });
       },
       child: Container(
         height: 50,
-        width:MediaQuery.of(context).size.width/2,
+        width: MediaQuery.of(context).size.width / 2,
         decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(25),
-          boxShadow:  [BoxShadow(
-                    color: Colors.black.withOpacity(0.19),
-                    offset: const Offset(0.5, 4),
-                    spreadRadius: 1,
-                    blurRadius: 8),
-                BoxShadow(
-                    color: Colors.white.withOpacity(0.4),
-                    offset: const Offset(-3, -4),
-                    spreadRadius: -2,
-                    blurRadius: 20),
-          ]
-        ),
+            color: onPressed ? newColor : Colors.white,
+            borderRadius: BorderRadius.circular(25),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black.withOpacity(0.19),
+                  offset: const Offset(0.5, 4),
+                  spreadRadius: 1,
+                  blurRadius: 8),
+              BoxShadow(
+                  color: Colors.white.withOpacity(0.4),
+                  offset: const Offset(-3, -4),
+                  spreadRadius: -2,
+                  blurRadius: 20),
+            ]),
         child: Center(
-          child: Text("Calculate",
-          style: GoogleFonts.lato(color: darkShrinePink, fontSize: 20),)),
+            child: Text(
+          "Calculate",
+          style: GoogleFonts.lato(color: darkShrinePink, fontSize: 20),
+        )),
       ),
     );
   }
